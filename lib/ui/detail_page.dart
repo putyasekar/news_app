dd77d21f863104ff6d2270b6d41afd4c16b831b4
Putya Sekar Ayu <5198258-putyasekar@users.noreply.gitlab.com>
2021-11-08 08:54:55 +0700

    detail

--- lib/ui/detail_page.dart
+++ lib/ui/detail_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/model/articles.dart';
+import 'package:news_app/utils/theme.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class DetailPage extends StatefulWidget {
   const DetailPage({Key? key, required this.article}) : super(key: key);
@@ -12,6 +14,91 @@ class DetailPage extends StatefulWidget {
 class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Image.network(
+              widget.article.urlToImage,
+              height: 350,
+              fit: BoxFit.cover,
+            ),
+            ListView(
+              children: [
+                SizedBox(height: 330),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(20)),
+                      color: Colors.white),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 24,
+                        ),
+                        Text(
+                          widget.article.title,
+                          style: titleArticle.copyWith(fontSize: 18),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(
+                                  Icons.account_circle,
+                                  size: 24,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  widget.article.author,
+                                  style: authorDateArticle.copyWith(
+                                    fontSize: 14,
+                                  ),
+                                )
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Icon(
+                                  Icons.calendar_today_outlined,
+                                  size: 24,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  timeUntil(DateTime.parse(
+                                      widget.article.publishedAt)),
+                                  style:
+                                      authorDateArticle.copyWith(fontSize: 14),
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  String timeUntil(DateTime parse) {
+    return timeago.format(parse, allowFromNow: true, locale: 'id');
   }
 }
